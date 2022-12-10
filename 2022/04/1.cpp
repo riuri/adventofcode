17cbb9a8a6b5241c81219feb04cd595343169da6
Iuri Rezende Souza <iuri.ena@gmail.com>
2022-12-10 10:26:31 -0500

    2022 day 04

---
+++ 2022/04/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int s1, s2, e1, e2, total = 0;
+  char d;
+  while (cin >> s1 >> d >> e1 >> d >> s2 >> d >> e2) {
+    if ((s1 <= s2 && e2 <= e1)
+      || (s2 <= s1 && e1 <= e2)) {
+      total++;
+    }
+  }
+  cout << total << endl;
+  return 0;
+}
